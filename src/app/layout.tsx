434a9b6ddec8f4c28b18223daacf251c7e65355c
tidvn <tiendung0325@gmail.com>
2024-06-13 15:51:33 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+// import Providers from '@/components/layout/providers';
+import { Toaster } from '@/components/ui/toaster';
+import '@uploadthing/react/styles.css';
+import type { Metadata } from 'next';
+import NextTopLoader from 'nextjs-toploader';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Next Shadcn',
+  description: 'Basic dashboard with Next.js and Shadcn'
+};
+
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} overflow-hidden`}>
+        <NextTopLoader />
+        {/* <Providers session={null}> */}
+          <Toaster />
+          {children}
+        {/* </Providers> */}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/layout/providers.tsx
@@ -0,0 +1,20 @@
+// 'use client';
+// import React from 'react';
+// import ThemeProvider from './ThemeToggle/theme-provider';
+// export default function Providers({
+//   session,
+//   children
+// }: {
+//   session: any;
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <>
+//       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+//         <SessionProvider session={session}>
+//           {children}
+//           </SessionProvider>
+//       </ThemeProvider>
+//     </>
+//   );
+// }
