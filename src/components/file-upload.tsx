ce0ffe288e205a5fe1ae3a9debcfe8edbff47951
tidvn <hello@tidnvn.com>
2024-06-17 13:01:07 +0700

    remove uploadthing

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,17 +1,14 @@
 'use client';
-import { OurFileRouter } from '@/app/api/uploadthing/core';
-import { UploadDropzone } from '@uploadthing/react';
 import { Trash } from 'lucide-react';
 import Image from 'next/image';
-import { UploadFileResponse } from 'uploadthing/client';
 import { IMG_MAX_LIMIT } from './forms/product-form';
 import { Button } from './ui/button';
 import { useToast } from './ui/use-toast';
 
 interface ImageUploadProps {
   onChange?: any;
-  onRemove: (value: UploadFileResponse[]) => void;
-  value: UploadFileResponse[];
+  onRemove: (data: any) => void;
+  value: any;
 }
 
 export default function FileUpload({
@@ -22,17 +19,17 @@ export default function FileUpload({
   const { toast } = useToast();
   const onDeleteFile = (key: string) => {
     const files = value;
-    let filteredFiles = files.filter((item) => item.key !== key);
+    let filteredFiles = files.filter((item: any) => item.key !== key);
     onRemove(filteredFiles);
   };
-  const onUpdateFile = (newFiles: UploadFileResponse[]) => {
+  const onUpdateFile = (newFiles: any) => {
     onChange([...value, ...newFiles]);
   };
   return (
     <div>
       <div className="mb-4 flex items-center gap-4">
         {!!value.length &&
-          value?.map((item) => (
+          value?.map((item: any) => (
             <div
               key={item.key}
               className="relative h-[200px] w-[200px] overflow-hidden rounded-md"
@@ -60,40 +57,7 @@ export default function FileUpload({
       </div>
       <div>
         {value.length < IMG_MAX_LIMIT && (
-          <UploadDropzone<OurFileRouter>
-            className="ut-label:text-sm ut-allowed-content:ut-uploading:text-red-300 py-2 dark:bg-zinc-800"
-            endpoint="imageUploader"
-            config={{ mode: 'auto' }}
-            content={{
-              allowedContent({ isUploading }) {
-                if (isUploading)
-                  return (
-                    <>
-                      <p className="mt-2 animate-pulse text-sm text-slate-400">
-                        Img Uploading...
-                      </p>
-                    </>
-                  );
-              }
-            }}
-            onClientUploadComplete={(res) => {
-              // Do something with the response
-              const data: UploadFileResponse[] | undefined = res;
-              if (data) {
-                onUpdateFile(data);
-              }
-            }}
-            onUploadError={(error: Error) => {
-              toast({
-                title: 'Error',
-                variant: 'destructive',
-                description: error.message
-              });
-            }}
-            onUploadBegin={() => {
-              // Do something once upload begins
-            }}
-          />
+          <div className="ut-label:text-sm ut-allowed-content:ut-uploading:text-red-300 py-2 dark:bg-zinc-800" />
         )}
       </div>
     </div>
