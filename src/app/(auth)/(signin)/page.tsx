f0f4cde34cfd044ffbde1e42e7d1f1a15315e5e4
tidvn <hello@tidnvn.com>
2024-06-17 17:00:26 +0700

    check hash

--- src/app/(auth)/(signin)/page.tsx
+++ src/app/(auth)/(signin)/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 import Link from 'next/link';
 import UserAuthForm from '@/components/forms/user-auth-form';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
---
+++ src/app/api/auth/validate-hash/route.ts
@@ -0,0 +1,32 @@
+import { isHashValid } from "@/lib/utils";
+import { isNil } from "lodash";
+
+export async function POST(request: Request) {
+    const { hash } = await request.json()
+    const botToken = process.env.BOT_TOKEN
+    if (isNil(hash)) {
+        return new Response('Missing required field hash', {
+            status: 400,
+        })
+    }
+
+    if (isNil(botToken)) {
+        return new Response('Internal server error', {
+            status: 500,
+        })
+    }
+
+    const data = Object.fromEntries(new URLSearchParams(hash));
+    const isValid = await isHashValid(data, botToken);
+
+    if (isValid) {
+        return new Response('Valid hash', {
+            status: 200,
+        })
+    }
+
+    return new Response('Invalid hash', {
+        status: 403,
+    })
+
+}
