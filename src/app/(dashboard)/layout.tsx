b91f4d4ce797eff23abd97cc62cd40083487d772
tidvn <tiendung0325@gmail.com>
2024-06-25 16:05:49 +0700

    fix bug

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 import BottomTab from '@/components/app/layout/bottom-tab';
 import Header from '@/components/app/layout/header';
-import Sidebar from '@/components/app/layout/sidebar';
 
 import React, { useEffect } from 'react';
 import { useTelegram } from '@/hooks/useTelegram';
@@ -24,7 +23,6 @@ export default function DashboardLayout({
         <>
             <Header />
             <div className="flex h-screen ">
-                <Sidebar />
                 <main className="flex-1 ">
                     <div className='pt-16 pb-24 px-4 md:px-8'>
                         {children}
