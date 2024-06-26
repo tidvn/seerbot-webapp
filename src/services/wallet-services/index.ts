3764f83b7f9450855b411f5a77a5f07527726f59
tidvn <tiendung0325@gmail.com>
2024-06-26 15:00:39 +0700

    fix

--- src/services/wallet-services/index.ts
+++ src/services/wallet-services/index.ts
@@ -2,13 +2,16 @@
 import { API_AUTH } from "@/constants/api.constants";
 import { ApiResponseInterface } from "@/models/app.model";
 import { AxiosBase } from "../base";
-import { useTelegram } from "@/hooks/useTelegram";
 
 const apiClient = new AxiosBase(API_AUTH);
 
-const getWalletInformation = async (): Promise<ApiResponseInterface> => {
-    const { tgData } = useTelegram();
-    return await apiClient.get(`/wallet`, { TelegramData: tgData })
+const getWalletInformation = async (tgData: string): Promise<ApiResponseInterface> => {
+    return await apiClient.get(`/wallet`, {
+        headers:
+        {
+            TelegramData: tgData,
+        },
+    })
 };
 
 export {
